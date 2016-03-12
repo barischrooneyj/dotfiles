04d48c7406081fb94f5d605f41ffd179aa31a597
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-12 19:46:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ echo alias cask="brew cask"
 echo alias vi="vim"
 EOM
 
-# brew
+# brew and cask
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install python3
+brew cleanup
 cask install google-chrome google-drive firefox vlc transmission flux Caskroom/versions/sublime-text3
 cask cleanup
