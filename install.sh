6042f92bda57dafad1f88409249670446506c3ae
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-12 19:19:17 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+source ~/.bash_profile
+cask install google-chrome google-drive firefox vlc transmission flux Caskroom/versions/sublime-text3
+cask cleanup
