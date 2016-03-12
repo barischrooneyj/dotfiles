6ec0fbbcfed4ad9f49e68c86423feebc53ddbb28
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-12 19:35:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+# .bash_profile
+bash_profile="$HOME/.bash_profile"
+touch "$bash_profile"
+echo "export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\"" >> "$bash_profile"
+echo "alias cask=\"brew cask\"" >> "$bash_profile"
+echo "alias vi=\"vim\"" >> "$bash_profile"
+
+# brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-source ~/.bash_profile
+brew install python3
 cask install google-chrome google-drive firefox vlc transmission flux Caskroom/versions/sublime-text3
 cask cleanup
