da8b44af79e5cf428dd91c9e30bf9fa8256f7e5e
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-12 21:22:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,12 @@ brew install python3 node
 brew cleanup
 cask install Caskroom/versions/sublime-text3 firefox flux google-chrome google-drive skype transmission vlc
 cask cleanup
+
+# github projects
+OIFS=$IFS
+IFS=/
+for i in gmassiac/project joeoh/gitmystats; do
+	user_repo=($i)
+	git clone https://github.com/${user_repo[0]}/${user_repo[1]} "$HOME/Documents/${user_repo[1]}"
+done
+IFS=$OIFS
