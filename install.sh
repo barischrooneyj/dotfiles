04225e84e07cfb441f2174bae086bfece5feffaf
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-13 00:03:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 set -euv
 
 # .bash_profile
-cat > "$HOME/.bash_profile" << EOM
+BASH_PROFILE = "$HOME/.bash_profile"
+cat > $BASH_PROFILE << EOM
+export CLICOLOR=1
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 alias cask="brew cask"
+alias ls="ls -a"
 alias vi="vim"
 EOM
+source $BASH_PROFILE
 
-# brew and cask
+# install applications
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install python3 node
 brew cleanup
@@ -19,7 +23,7 @@ cask cleanup
 OIFS=$IFS
 IFS=/
 for i in gmassiac/project joeoh/gitmystats; do
-	user_repo=($i)
-	git clone https://github.com/${user_repo[0]}/${user_repo[1]} "$HOME/Documents/${user_repo[1]}"
+  user_repo=($i)
+  git clone https://github.com/${user_repo[0]}/${user_repo[1]} "$HOME/Documents/${user_repo[1]}"
 done
 IFS=$OIFS
