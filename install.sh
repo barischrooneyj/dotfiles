319ffb47c7670a086ddd1fd8e8aec8031176b052
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-05-29 21:40:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
 set -euv
 
 # .bash_profile
-cat > ${HOME}/.bash_profile << EOM
-export CLICOLOR=1
+bash_profile=${HOME}/.bash_profile 
+cat > $bash_profile << EOM
 alias cask="brew cask"
 alias ls="ls -a"
 alias vi="vim"
+export CLICOLOR=1
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 EOM
-source ${HOME}/.bash_profile
+source $bash_profile
 
 # install applications
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python3 node
+brew install node python3 wget
 brew cleanup
-cask install Caskroom/versions/sublime-text3 firefox flux google-chrome google-drive homebrew/science/octave skype transmission vlc
+cask install Caskroom/versions/sublime-text3 firefox flux google-chrome google-drive skype transmission vlc
 cask cleanup
 
 # github projects
 OIFS=$IFS
 IFS=/
-for i in barischj/spin; do
-  user_repo=($i)
+for user_repo in barischj/spin; do
   git clone https://github.com/${user_repo[0]}/${user_repo[1]} ${HOME}/Documents/${user_repo[1]}
 done
 IFS=$OIFS
