2e131c67dbea3552a0a6faa5a1f0158a3b20a510
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-05-31 18:13:11 +0100

    Fix #1

--- install.sh
+++ install.sh
@@ -1,27 +1,39 @@
-set -euv
+set -eu
 
-# .bash_profile
-bash_profile=${HOME}/.bash_profile 
-cat > $bash_profile << EOM
-alias cask="brew cask"
-alias ls="ls -a"
-alias vi="vim"
-export CLICOLOR=1
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-EOM
-source $bash_profile
+# ~/.bash_profile
+bash_profile_path=${HOME}/.bash_profile
+bash_profile_lines=(
+	"alias ls=\"ls -a\""
+	"alias vi=\"vim\""
+	"export CLICOLOR=1"
+	"export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\""
+)
+for line in "${bash_profile_lines[@]}"; do
+	grep -q "$line" "$bash_profile_path" || echo "$line" >> "$bash_profile_path"
+done
+source "$bash_profile_path"
 
 # install applications
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install node python3 wget
-brew cleanup
-cask install Caskroom/versions/sublime-text3 firefox flux google-chrome google-drive skype transmission vlc
-cask cleanup
+case "$OSTYPE" in
+ 	darwin*)
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || true
+		brew install node python3 wget
+		brew cleanup
+		brew cask install Caskroom/versions/sublime-text3 firefox flux \
+						  				google-chrome google-drive skype transmission vlc
+		brew cask cleanup
+	;; 
+ 	linux*)
+		echo "TODO install Linux apps"
+	;;
+	*)
+		echo "unknown: $OSTYPE"
+	;;
+esac
 
 # github projects
-OIFS=$IFS
-IFS=/
-for user_repo in barischj/spin; do
+repo_paths="barischj/me barischj/spin"
+for i in $repo_paths; do
+	user_repo=($(echo $i | tr "/" "\n"))
   git clone https://github.com/${user_repo[0]}/${user_repo[1]} ${HOME}/Documents/${user_repo[1]}
 done
-IFS=$OIFS
