ed1bf37ae7c5e21cfb371edf06f52d0e7dc815da
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-06-02 15:31:47 +0100

    Added ghc and haskell-stack

--- install.sh
+++ install.sh
@@ -3,37 +3,38 @@ set -eu
 # ~/.bash_profile
 bash_profile_path=${HOME}/.bash_profile
 bash_profile_lines=(
-	"alias ls=\"ls -a\""
-	"alias vi=\"vim\""
-	"export CLICOLOR=1"
-	"export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\""
+  "alias ls=\"ls -a\""
+  "alias vi=\"vim\""
+  "export CLICOLOR=1"
+  "export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\""
 )
 for line in "${bash_profile_lines[@]}"; do
-	grep -q "$line" "$bash_profile_path" || echo "$line" >> "$bash_profile_path"
+  grep -q "$line" "$bash_profile_path" || echo "$line" >> "$bash_profile_path"
 done
 source "$bash_profile_path"
 
 # install applications
 case "$OSTYPE" in
- 	darwin*)
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || true
-		brew install node python3 wget
-		brew cleanup
-		brew cask install Caskroom/versions/sublime-text3 firefox flux \
-						  				google-chrome google-drive skype transmission vlc
-		brew cask cleanup
-	;; 
- 	linux*)
-		echo "TODO install Linux apps"
-	;;
-	*)
-		echo "unknown: $OSTYPE"
-	;;
+  darwin*)
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || true
+    brew install node python3 wget
+    brew cleanup
+    brew cask install Caskroom/versions/sublime-text3 firefox flux \
+                      ghc google-chrome google-drive haskell-stack skype
+                      transmission vlc
+    brew cask cleanup
+  ;; 
+  linux*)
+    echo "TODO install Linux apps"
+  ;;
+  *)
+    echo "unknown: $OSTYPE"
+  ;;
 esac
 
 # github projects
 repo_paths="barischj/me barischj/spin"
 for i in $repo_paths; do
-	user_repo=($(echo $i | tr "/" "\n"))
+  user_repo=($(echo $i | tr "/" "\n"))
   git clone https://github.com/${user_repo[0]}/${user_repo[1]} ${HOME}/Documents/${user_repo[1]}
 done
