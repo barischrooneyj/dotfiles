2f5ac1d495a5fcfd6c2a0371f7581006a08b246b
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-09-13 00:16:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,28 @@
-set -euo pipefail
+set -eou pipefail
 
-# ~/.bash_profile
-bash_profile_path=${HOME}/.bash_profile
-bash_profile_lines=(
-  "alias ls=\"ls -a\""
-  "alias vi=\"vim\""
-  "export CLICOLOR=1"
-  "export HOMEBREW_CASK_OPTS=\"--appdir=/Applications\""
+# Install applications with Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ||:
+brew install bash ghc haskell-stack node python3 wget
+brew cleanup
+brew cask install firefox flux google-chrome google-drive skype sublime-text transmission vlc
+brew cask cleanup
+
+# Update RC files
+filemap=(
+    "barischj/dotfiles/master/.bashrc $HOME/.bashrc"
 )
-for line in "${bash_profile_lines[@]}"; do
-  grep -q "$line" "$bash_profile_path" || echo "$line" >> "$bash_profile_path"
-done
-source "$bash_profile_path"
 
-# install applications
-case "$OSTYPE" in
-  darwin*)
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || true
-    brew install ghc haskell-stack node python3 wget
-    brew cleanup
-    brew cask install Caskroom/versions/sublime-text3 firefox flux \
-                      google-chrome google-drive skype transmission vlc
-    brew cask cleanup
-  ;; 
-  linux*)
-    echo "TODO install Linux apps"
-  ;;
-  *)
-    echo "unknown: $OSTYPE"
-  ;;
-esac
+for line in "${filemap[@]}"; do
+    read src_path dest_path <<< $line
+    src_file="`wget -qO- https://raw.githubusercontent.com/$src_path`"
+    IFS=$'\n' read -rd '' -a src_lines <<< "$src_file"
+    touch "$dest_path"
+    for src_line in "${src_lines[@]}"; do
+        grep -q "$src_line" "$dest_path" || echo "$src_line" >> "$dest_path"
+    done
+done
+
+source "$HOME/.bashrc"
 
 # Clone GitHub projects to ~/Documents
 while true; do
@@ -37,9 +30,13 @@ while true; do
     if [[ $input = "" ]]; then break; fi
     user_repo=($(echo $input | tr "/" " "))
     if (( ${#user_repo[@]} != 2 )); then
-        echo "Input in incorrect format";
+        echo "Input format incorrect";
     else
         git clone https://github.com/${user_repo[0]}/${user_repo[1]} \
-        ${HOME}/Documents/${user_repo[0]}/${user_repo[1]}
+        ${HOME}/Documents/${user_repo[0]}_${user_repo[1]}
     fi
 done
+
+# Change shell to bash 4.
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+chsh -s /usr/local/bin/bash
