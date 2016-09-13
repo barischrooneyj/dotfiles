192cf283d906eac3199873290b7beb9d4dcad748
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-09-13 21:52:58 +0200

    Configure dock and set .bash_profile

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 set -eou pipefail
 
+# Configure dock.
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock orientation -string left
+defaults delete com.apple.dock persistent-apps
+killall Dock
+
 # Install applications with Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ||:
 brew install bash ghc haskell-stack node python3 wget
@@ -9,16 +15,16 @@ brew cask cleanup
 
 # Update config files
 filemap=(
-    "barischj/dotfiles/master/.bashrc $HOME/.bashrc"
-    "barischj/dotfiles/master/Preferences.sublime-settings \
+    ".bash_profile $HOME/.bash_profile"
+    ".bashrc $HOME/.bashrc"
+    "Preferences.sublime-settings \
         $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings"
 )
 for line in "${filemap[@]}"; do
     read src_path dest_path <<< $line
-    src_file="`wget -qO- https://raw.githubusercontent.com/$src_path`"
+    src_file="`wget -qO- https://raw.githubusercontent.com/barischj/dotfiles/master/$src_path`"
     echo "$src_file" > "$dest_path"
 done
-source "$HOME/.bashrc"
 
 # Clone GitHub repos
 msg='Enter GitHub repo to clone e.g. barischj/dotfiles or return to continue: '
