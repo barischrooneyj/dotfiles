2b65918f042f2c1e6d8a0c2b5ab29a385d90297d
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2017-08-18 01:38:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,50 +13,32 @@ defaults write -g KeyRepeat -int 1
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Xcode command line tools
+# TODO: Possibly covered by installing brew
 xcode-select --install ||:
  
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ||:
-
-# Homebrew installs
-brew install bash bash-completion@2 git-flow-avh haskell-stack ispell mas node python3
-brew cask install firefox franz google-chrome google-drive iterm2 spotify sublime-text transmission vlc
-
-# Xcode
-read -p $'Install Xcode?\n' -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    mas signin --dialog barischj@tcd.ie ||:
-    mas install `mas search Xcode | head -n 1 | cut -f 1 -d ' '`
-    sudo xcodebuild -license accept
-fi
+brew install bash bash-completion@2 duti emacs git-flow-avh haskell-stack ispell python3 tmux
+brew cask install google-backup-and-sync google-chrome iina iterm2 spotify transmission
+brew cleanup
+brew cask cleanup
 
 # Fira Code font
 brew tap caskroom/fonts
 brew cask install font-fira-code
 
 # Spacemacs
-rm -rf ~/.*emacs*
-brew tap d12frosted/emacs-plus
-brew install emacs-plus
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 ## haskell layer deps
 stack install --install-ghc apply-refact hlint stylish-haskell hasktags hoogle intero
-## javascript layer deps
-npm install -g eslint js-beautify tern
 ## python layer deps
 pip install autoflake flake8 hy
-## react layer deps
-npm install -g babel-eslint eslint eslint-plugin-react js-beautify tern
 
 # Update config files
 filemap=(  # {relative url: absolute path}
     ".bash_profile $HOME/.bash_profile"
     ".bashrc $HOME/.bashrc"
     ".spacemacs $HOME/.spacemacs"
-    "Preferences.sublime-settings \
-        $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings"
 )
 for line in "${filemap[@]}"; do
     read src_path dest_path <<< $line
@@ -68,11 +50,10 @@ for line in "${filemap[@]}"; do
 done
 
 # Change default apps
-brew install duti
 defaults=(
-    "m4a org.videolan.vlc"
-    "mkv org.videolan.vlc"
-    "mp4 org.videolan.vlc"
+    "m4a com.colliderli.iina"
+    "mkv com.colliderli.iina"
+    "mp4 com.colliderli.iina"
 )
 for line in "${defaults[@]}"; do
     read format app <<< $line
@@ -80,22 +61,11 @@ for line in "${defaults[@]}"; do
 done
 
 # Open apps
-open -a emacs
-open /Applications/Google\ Chrome.app
-open /Applications/Google\ Drive.app
+open -a backup\ and\ sync
+open -a flux
 
-# Cleanup
-brew cleanup
-brew cask cleanup
-
-# Bug fixes
-cat << EOM
-
-For anaconda-mode errors see:
-https://github.com/syl20bnr/spacemacs/tree/master/layers/%2Blang/python#auto-completion-anaconda-dependencies
-https://github.com/syl20bnr/spacemacs/issues/8412
-
-EOM
+# Final steps
+echo 'Remaining issues: https://github.com/barischrooneyj/dotfiles/issues'
 
 # Install "all appropriate" updates
 softwareupdate --install --all
