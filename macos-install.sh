edaed4375ed8c84cf9bb997506979d94e831520f
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-05 00:22:29 +0100

    Copy fonts into place

--- macos-install.sh
+++ macos-install.sh
@@ -17,23 +17,20 @@ applescripts=(
     'highlight-colour.scpt'
 )
 for filename in "${applescripts[@]}"; do
-    url="https://raw.githubusercontent.com/barischj/dotfiles/master/applescript/$filename"
-    src_file="$(curl -fsSL $url)"
     echo "Running $filename"
-    echo "$src_file" > temp.scpt
-    osascript temp.scpt
+    url="https://raw.githubusercontent.com/barischj/dotfiles/master/applescript/$filename"
+    curl $url | osascript
 done
-rm temp.scpt
 
 # Install apps with Homebrew.
 if hash brew 2>/dev/null; then
-  echo 'brew is already installed'
+  echo 'Homebrew is already installed'
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 brew tap d12frosted/emacs-plus
 brew tap homebrew/cask-fonts
-brew install duti emacs@28 pyenv reattach-to-user-namespace tmux
+brew install duti emacs@28 pyenv reattach-to-user-namespace tmux wget
 brew cask install firefox-developer-edition flux font-iosevka font-iosevka-slab google-backup-and-sync google-chrome iterm2 signal spotify sublime-text transmission vlc
 brew install openssl readline sqlite3 xz zlib # pyenv: https://github.com/pyenv/pyenv/wiki#suggested-build-environment
 
@@ -43,7 +40,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 # Doom Emacs.
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
-brew install fd findutils ripgrep
+brew install fd findutils ripgrep  # https://github.com/hlissner/doom-emacs#prerequisites
 
 # Move dotfiles into place.
 dotfiles=(
@@ -62,7 +59,6 @@ done
 # Oh My Zsh.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
-(cd $ZSH_CUSTOM/themes/typewritten && git pull && cd -)
 source ~/.zshrc
 
 # Tmux Plugin Manager.
@@ -70,10 +66,10 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Set default apps.
 defaults=(
-    "avi org.videolan.vlc"
-    "m4a org.videolan.vlc"
-    "mkv org.videolan.vlc"
-    "mp4 org.videolan.vlc"
+    'avi org.videolan.vlc'
+    'm4a org.videolan.vlc'
+    'mkv org.videolan.vlc'
+    'mp4 org.videolan.vlc'
 )
 for line in "${defaults[@]}"; do
     read extension app <<< "$line"
@@ -82,30 +78,21 @@ for line in "${defaults[@]}"; do
 done
 
 # Iosevka font.
-# ~/Library/Fonts
+wget -r https://github.com/barischrooneyj/dotfiles/tree/master/fonts/iosevka-term-slab-haskell
+for f in dotfiles/fonts/**/*.ttf; do cp $f ~/Library/Fonts; done
 
 # Cleanup.
 brew cleanup
 
 # Open apps that require further action.
 open -a 'firefox developer edition'
-read -n 1 -s -r -p "Press any key to continue"
+read -n 1 -s -r -p 'Press any key to continue'
 open -a 'backup and sync'
-read -n 1 -s -r -p "Press any key to continue"
+read -n 1 -s -r -p 'Press any key to continue'
 open -a flux
-
-# Final instructions.
-cat << EOM
-
-|-----------------------------------------------------------------------|
-|                                                                       |
-| * Remaining issues: https://github.com/barischrooneyj/dotfiles/issues |
-|                                                                       |
-| * Reboot once updates are installed for changes to take effect.       |
-|                                                                       |
-|-----------------------------------------------------------------------|
-
-EOM
+read -n 1 -s -r -p 'Press any key to continue'
+open https://github.com/barischrooneyj/dotfiles/issues
+read -n 1 -s -r -p 'Press any key to install updates and restart'
 
 # Install "all appropriate" updates.
 softwareupdate --install --all --restart
