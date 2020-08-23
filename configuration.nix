605d19ad6d41adf82499d5d3a06539b50c7b6373
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-23 23:37:57 +0100

    Newer Firefox

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,11 @@
 let
   unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
 in {
+  # Allow unfree software.
+  nixpkgs.config.allowUnfree = true;
   # Garbage collect old generations.
   nix.gc.automatic = true;
   nix.gc.dates = "--delete-older-than 7d";
-  # Allow proprietary or unfree software
-  nixpkgs.config.allowUnfree = true;
   # Include the results of the hardware scan.
   imports = [ ./hardware-configuration.nix ];
   # Use the systemd-boot EFI boot loader.
@@ -15,9 +15,6 @@ in {
   # Per-interface useDHCP will be mandatory in the future.
   networking.useDHCP = false;
   networking.interfaces.ens33.useDHCP = true;
-  # Sound please.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
   # Localisation.
   i18n.defaultLocale = "en_US.UTF-8";
   console.font = "Fira Code Retina";
@@ -25,9 +22,10 @@ in {
   time.timeZone = "Europe/Dublin";
   # Packages.
   environment.systemPackages = with pkgs; [
-    cava compton curl emacs feh firefox-devedition-bin haskellPackages.xmobar
-    git kitty lsd neofetch neovim rofi spotifyd starship tuir tmux wget xclip
-    unstable.spotify-tui
+    cava compton curl emacs feh haskellPackages.xmobar git kitty lsd neofetch
+    neovim rofi spotifyd starship tuir tmux wget xclip
+
+    unstable.firefox-devedition-bin-unwrapped unstable.spotify-tui
   ];
   programs.fish.enable = true;
   fonts.fonts = with pkgs; [ fira-code ];
@@ -41,6 +39,9 @@ in {
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.user = "jeremy";
+  # Sound and bluetooth.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
   # VMware.
   virtualisation.vmware.guest.enable = true;
   # User account.
