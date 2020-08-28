a994ec5297827a1f24523ea41bbfa7e449f7238e
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-28 01:54:07 +0100

    Add webcam support

--- configuration.nix
+++ configuration.nix
@@ -3,12 +3,17 @@
 let
   unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
 in {
+  # NixOS version.
+  system.stateVersion = "20.03";
   # Allow unfree software.
   nixpkgs.config.allowUnfree = true;
   # Garbage collect old generations.
   nix.gc.automatic = true;
   nix.gc.dates = "--delete-older-than 7d";
-  # Imports. Include results of the hardware scan.
+  ############
+  # HARDWARE #
+  ############
+  # Include results of the hardware scan.
   imports = [ ./hardware-configuration.nix ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -21,14 +26,45 @@ in {
   networking.wireless.networks = {
     "VODAFONE-2.4G" = { psk = "pass"; };
   };
-  # Localisation.
-  i18n.defaultLocale = "en_IE.UTF-8";
+  # Keyboard.
   services.xserver.layout = "gb";
   services.xserver.xkbVariant = "mac";
   services.xserver.xkbOptions = "caps:escape";
   console.useXkbConfig = true;
-  time.timeZone = "Europe/Dublin";
-  # Packages.
+  # Trackpad.
+  services.xserver.libinput.enable = true;
+  services.xserver.multitouch.enable = true;
+  services.xserver.multitouch.invertScroll = true;
+  # Sound and bluetooth.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  # Webcam.
+  hardware.facetimehd.enable = true;
+  ###################
+  # ACCOUNT & LOGIN #
+  ###################
+  users.users.jeremy.isNormalUser = true;
+  users.users.jeremy.extraGroups = [ "wheel" ];
+  users.users.jeremy.shell = pkgs.fish;
+  # Skip the login screen.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.autoLogin.enable = true;
+  services.xserver.displayManager.lightdm.autoLogin.user = "jeremy";
+  #######################
+  # DESKTOP ENVIRONMENT #
+  #######################
+  # X11 windowing system.
+  services.xserver.enable = true;
+  # Xmonad window manager.
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+  services.xserver.displayManager.defaultSession = "none+xmonad";
+  ############
+  # PACKAGES #
+  ############
   environment.systemPackages = with pkgs; [
     bitwarden-cli cabal-install cabal2nix cava curl emacs feh
     haskellPackages.xmobar home-manager git kitty lsd neofetch neovim rofi
@@ -36,30 +72,11 @@ in {
     # Unstable packages.
     unstable.firefox-devedition-bin-unwrapped unstable.spotify-tui
   ];
-  programs.fish.enable = true;
   fonts.fonts = with pkgs; [ fira-code montserrat ];
-  # Sound and bluetooth.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  # X11 windowing system.
-  services.xserver.enable = true;
-  # Xmonad window manager.
-  services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
-  # Skip the login screen.
-  services.xserver.displayManager.defaultSession = "none+xmonad";
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.lightdm.autoLogin.enable = true;
-  services.xserver.displayManager.lightdm.autoLogin.user = "jeremy";
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-  # User account.
-  users.users.jeremy.isNormalUser = true;
-  users.users.jeremy.extraGroups = [ "wheel" ];
-  users.users.jeremy.shell = pkgs.fish;
-  # NixOS version.
-  system.stateVersion = "20.03"; # Did you read the comment?
+  programs.fish.enable = true;
+  ################
+  # LOCALISATION #
+  ################
+  i18n.defaultLocale = "en_IE.UTF-8";
+  time.timeZone = "Europe/Dublin";
 }
