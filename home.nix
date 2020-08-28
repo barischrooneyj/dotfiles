9132191525152c4c155d1c0b20eae73111902ee7
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-28 03:11:39 +0100

    Theme kitty terminal emulator

--- home.nix
+++ home.nix
@@ -5,6 +5,9 @@
   home.file.".doom.d/config.el".source        = "/home/jeremy/dotfiles/doom/config.el";
   home.file.".doom.d/packages.el".source      = "/home/jeremy/dotfiles/doom/packages.el";
   home.file.".config/nvim/init.vim".source    = "/home/jeremy/dotfiles/nvim/init.vim";
+  home.file.".config/kitty/kitty.conf".source = "/home/jeremy/dotfiles/kitty/kitty.conf";
+  home.file.".config/kitty/dracula.conf".source = "/home/jeremy/dotfiles/kitty/dracula.conf";
+  home.file.".config/kitty/diff.conf".source = "/home/jeremy/dotfiles/kitty/diff.conf";
   home.file.".xmonad/xmonad.hs".source        = "/home/jeremy/dotfiles/xmonad/xmonad.hs";
   home.file.".config/xmobar/xmobar.hs".source = "/home/jeremy/dotfiles/xmobar/xmobar.hs";
   programs.git = {
