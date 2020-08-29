de00b41471376c67a04ec496e37c84bea92de625
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-08-29 13:47:11 +0100

    Delete up-dotfiles.sh

--- up-dotfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp /home/jeremy/.config/nixpkgs/home.nix /home/jeremy/dotfiles/home.nix
-cp /etc/nixos/configuration.nix          /home/jeremy/dotfiles/configuration.nix
-git commit -a
-git push
