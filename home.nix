bb8037180bf531bc2ce55a883931c5882bf39cd7
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-29 14:00:07 +0100

    Add starship config

--- home.nix
+++ home.nix
@@ -4,13 +4,14 @@
   home.file.".config/bg.jpg".source           = "/home/jeremy/dotfiles/background.jpg";
   home.file.".doom.d/config.el".source        = "/home/jeremy/dotfiles/doom/config.el";
   home.file.".doom.d/packages.el".source      = "/home/jeremy/dotfiles/doom/packages.el";
-  home.file.".config/nvim/init.vim".source    = "/home/jeremy/dotfiles/nvim/init.vim";
+  home.file.".config/fish/config.fish".source = "/home/jeremy/dotfiles/fish/config.fish";
   home.file.".config/kitty/kitty.conf".source = "/home/jeremy/dotfiles/kitty/kitty.conf";
   home.file.".config/kitty/dracula.conf".source = "/home/jeremy/dotfiles/kitty/dracula.conf";
   home.file.".config/kitty/diff.conf".source = "/home/jeremy/dotfiles/kitty/diff.conf";
-  home.file.".config/fish/config.fish".source = "/home/jeremy/dotfiles/fish/config.fish";
-  home.file.".xmonad/xmonad.hs".source        = "/home/jeremy/dotfiles/xmonad/xmonad.hs";
+  home.file.".config/nvim/init.vim".source    = "/home/jeremy/dotfiles/nvim/init.vim";
+  home.file.".config/starship.toml".source    = "/home/jeremy/dotfiles/starship/starship.toml";
   home.file.".config/xmobar/xmobar.hs".source = "/home/jeremy/dotfiles/xmobar/xmobar.hs";
+  home.file.".xmonad/xmonad.hs".source        = "/home/jeremy/dotfiles/xmonad/xmonad.hs";
   programs.git = {
     enable = true;
     userName = "barischrooneyj";
